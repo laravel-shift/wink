b30c96e4a378f81103614e788194c86f0f1cf271
= <=>
2024-10-14 14:28:48 +0500

    udpated the Authenticateable contract for wink author

--- src/WinkAuthor.php
+++ src/WinkAuthor.php
@@ -101,6 +101,16 @@ class WinkAuthor extends AbstractWinkModel implements Authenticatable
     }
 
     /**
+     * Get the name of the password field.
+     *
+     * @return string
+     */
+    public function getAuthPasswordName()
+    {
+        return 'password';
+    }
+
+    /**
      * Get the unique identifier for the user.
      *
      * @return mixed
